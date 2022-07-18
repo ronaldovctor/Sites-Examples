dfdfb7cd51edfe3da8364e70f1fadc57e801fde4
Ronaldo <ronaldovctor@outlook.com>
2022-07-18 13:41:01 -0300

    Food site

---
+++ food-site/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', ()=> {
+    const header = document.querySelector('header')
+    header.classList.toggle('sticky', window.scrollY > 3)
+    document.querySelector('.menuToggle').classList.toggle('sticky', (header.classList.contains('sticky') && window.scrollY > 3))
+})
+
+function toggleMenu(){
+    const menuToggle = document.querySelector('.menuToggle')
+    const navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
