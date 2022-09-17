b7b304625c27301484b283e27de95aa322764205
Ronaldo <ronaldovctor@outlook.com>
2022-09-17 14:49:22 -0300

    Adding header

--- sweets-d/src/App.tsx
+++ sweets-d/src/App.tsx
@@ -1,7 +1,21 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.scss'
+import Footer from './page/footer/Footer'
+import Header from './page/header/Header'
+import Home from './page/home/Home'
 
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path='/' element={<Home />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	)
 }
 
 export default App
---
+++ sweets-d/src/page/header/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styles from './Header.module.scss'
+import { ReactComponent as SweetsD } from '../../assets/logo.svg'
+
+function Header() {
+	return (
+		<header className={styles.header}>
+			<nav className={`${styles.nav} container`}>
+				<div className={styles.logo}>
+					<SweetsD />
+				</div>
+				<div className={styles.links}>
+					<ul>
+						<li>
+							<NavLink to='#sabores'>Sabores</NavLink>
+						</li>
+						<li>
+							<NavLink to='/pedidos'>Pedidos</NavLink>
+						</li>
+						<li>
+							<NavLink to='#contato'>Contato</NavLink>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+	)
+}
+
+export default Header
